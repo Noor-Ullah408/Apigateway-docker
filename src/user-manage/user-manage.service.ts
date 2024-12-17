4de26dc23f98c7386439df55d824161d66a7527b
Noor Ullah Al Noor <noor.dcastalia@gmail.com>
2024-12-17 16:31:11 +0600

    added logs to see interaction from users.service file

--- src/user-manage/user-manage.service.ts
+++ src/user-manage/user-manage.service.ts
@@ -7,21 +7,25 @@ export class UserManageService {
   constructor(@Inject('USERS_MANAGE') private TCPClient: ClientProxy) {}
   createUser(createUser: CreateUserDto) {
     const pattern = { cmd: 'create_user' };
+    console.log('Firing createUser()');
     return this.TCPClient.send(pattern, createUser);
   }
 
   findAll() {
     const pattern = { cmd: 'getAll_users' };
+    console.log('Firing findAll()');
     return this.TCPClient.send(pattern, '');
   }
 
   findOne(id: number) {
     const pattern = { cmd: 'getOne_user' };
+    console.log('Firing findOne()');
     return this.TCPClient.send(pattern, id);
   }
 
   update(id: number, updateUser: CreateUserDto) {
     const pattern = { cmd: 'update_user' };
+    console.log('Firing update()');
     const payload = {
       id,
       updateUser,
